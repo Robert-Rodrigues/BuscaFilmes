deba207d32aeb76989bfac88bbca121e47af7e57
Robert <robert__souza@hotmail.com>
2023-08-05 21:47:57 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+const NavBar = () => {
+  return (
+    <nav id='navbar'>
+      <h2>
+        <Link to="/"><BiCameraMovie />BuscaFilmes</Link>
+      </h2>
+      <form>
+        <input type="text" placeholder='Procure um filme' />
+        <button type='submit'><BiSearchAlt2 /></button>
+      </form>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [mainMovies, setMainMovies] = useState([])
+
+  const getMainRatedMovies = async (url) => {
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMainMovies(data.results);
+  };
+  
+  useEffect(() => {
+
+    const mainRatedMoviesURL = `${moviesURL}top_rated?${apiKey}`;
+    getMainRatedMovies(mainRatedMoviesURL)
+  }, [])
+  return (
+    <div className='container'>
+      <h2 className='title'>Filmes mais bem avaliados:</h2>
+      <div className="movies-container">
+      {mainMovies.length === 0 && <p>Carregando...</p>}
+      {mainMovies.length > 0 && mainMovies.map((movie) => <p>{movie.title}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default Home
