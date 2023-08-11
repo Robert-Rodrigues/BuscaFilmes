4db8c87783a11cda9d1d40c8655e274185dd1195
Robert <robert__souza@hotmail.com>
2023-08-10 23:51:14 -0300

    second commit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+import './MovieGrid.css';
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='container'>
+      <h2 className='title'>Resultados para:<span className="query-text">"{query}"</span></h2>
+      <div className="movies-container">
+      {mainMovies.length === 0 && <p>Carregando...</p>}
+      {mainMovies.length > 0 && mainMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
   )
 }
 
